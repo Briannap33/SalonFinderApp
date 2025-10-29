d284534b12a3b637e5ad62d876252747d2380790
Briannap33 <20102669@mail.wit.ie>
2025-10-29 22:23:23 +0000

    files added, logging event handling done, debugged

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,15 @@
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
 }
 
 android {
-    namespace = "com.setu.salonfinderapp"
-    compileSdk {
-        version = release(36)
-    }
+    namespace = "com.setu.salonFinderApp"
+    compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.setu.salonfinderapp"
+        applicationId = "com.setu.salonFinderApp"
         minSdk = 30
         targetSdk = 36
         versionCode = 1
@@ -29,20 +28,22 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    kotlin {
+        jvmToolchain(17)
     }
 }
 
 dependencies {
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.timber)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/setu/salonfinderapp/SalonActivity.kt
+++ app/src/main/java/com/setu/salonfinderapp/SalonActivity.kt
@@ -5,18 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.setu.salonFinderApp.R
+import timber.log.Timber
 
 class SalonActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_salon)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        Timber.plant(Timber.DebugTree())
+        Timber.i("Salon Activity started..")
         }
     }
-}
+
 
 
