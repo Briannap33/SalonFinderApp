17196e50ca0d4d2982d961e48a018ac92c2ca763
Briannap33 <20102669@mail.wit.ie>
2025-11-05 12:19:59 +0000

    cancel button added, debugging to get it to work

--- app/src/main/java/com/setu/salonfinderapp/activities/SalonActivity.kt
+++ app/src/main/java/com/setu/salonfinderapp/activities/SalonActivity.kt
@@ -1,8 +1,11 @@
 package com.setu.salonfinderapp.activities
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
+import com.setu.salonfinderApp.R
 import com.setu.salonfinderApp.databinding.ActivitySalonBinding
 import com.setu.salonfinderapp.main.MainApp
 import com.setu.salonfinderapp.models.SalonModel
@@ -19,25 +22,35 @@ class SalonActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySalonBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbarAdd)
+
 
         app = application as MainApp
-        i("Salon Activity Started")
+
         binding.btnAdd.setOnClickListener() {
             salonEntry.name = binding.salonName.text.toString()
             salonEntry.description = binding.description.text.toString()
             if (salonEntry.name.isNotEmpty()) {
                 app.salonList.add(salonEntry.copy())
-                i("add Button Pressed: ${salonEntry}")
-                for (i in app.salonList.indices) {
-                    i("Placemark[$i]:${this.app.salonList[i]}")
-                }
                 setResult(RESULT_OK)
                 finish()
-            } else {
-                Snackbar.make(it, "Please Enter a salon name", Snackbar.LENGTH_LONG)
+            }
+            else {
+                Snackbar.make(it,"Please Enter a Name", Snackbar.LENGTH_LONG)
                     .show()
             }
         }
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_salon, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_cancel -> { finish() }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
 
